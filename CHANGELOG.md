dd7467f4775b4f6cf1ba047bd5faea8992c41a54
Son Tran <286.trants@gmail.com>
2024-06-12 16:08:38 +0700

    Add CHANGELOG.md, version.go

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v1.0.0
+
+### Features
+* feat(structs): provides functions for struct information retrieving.
+* feat(times): provides functionality for measuring and displaying time.
---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2022-2024 The Focela Authors, All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gocarp
+
+// Version is the current gocarp version.
+const Version = "v1.0.0"
