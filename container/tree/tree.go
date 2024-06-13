6f90cfb5cf48132874a32893cea1c3c154e2d06d
Son Tran <286.trants@gmail.com>
2024-06-13 08:32:23 +0700

    feat(tree): implement tree package - Package `tree` provides concurrent-safe/unsafe tree containers

---
+++ container/tree/tree.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2022-2024 The Focela Authors, All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package tree provides concurrent-safe/unsafe tree containers.
+//
+// Some implements are from: https://github.com/emirpasic/gods
+package tree
